797a424dbb99fe6f66e2684f46f7e4d7e5dcd2f2
Nicola Cordioli <cordioli.nicola@gmail.com>
2018-08-04 00:58:48 +0200

    first commit

---
+++ src/components/BestPractice.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h3>{{bp.title}}:</h3>
+        {{bp.note}}
+    </div>    
+</template>
+<script>
+export default {
+  props: ["bp"]
+};
+</script>
+<style>
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <best-practice v-for="(i, index) in list" :bp="i" :key="index">
+    </best-practice>
+    <br>
+    <div>
+      <input type="text" name="newBpTitle" id="newBpTitle" v-model="newBpTitle">
+      <input type="text" name="newBpNote" id="newBpNote" v-model="newBpNote">
+      <button name="add" @click="add">Add</button>
+    </div>
+  </div>
+</template>
+<script>
+import bestPractice from "./BestPractice.vue";
+
+export default {
+  data() {
+    return {
+      newBpTitle: "",
+      newBpNote: "",
+      list: [
+        { title: "Teeth", note: "Remember to wash them every day!" },
+        { title: "Be nice", note: 'Always say "Please" and "Thank you"' }
+      ]
+    };
+  },
+  methods: {
+    add() {
+      if (this.newBpTitle !== "" && this.newBpNote !== "") {
+        this.list.push({ title: this.newBpTitle, note: this.newBpNote });
+        this.newBpTitle = "";
+        this.newBpNote = "";
+      } else {
+        alert("Titolo e note sono obbligatori");
+      }
+    }
+  },
+  components: {
+    bestPractice
+  }
+};
+</script>
+<style>
+</style>
